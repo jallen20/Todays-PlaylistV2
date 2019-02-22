987fab061e6dd6cd2b3874e70138bbf9d7f3fd49
ceesvr <allen.justin78@gmail.com>
2019-02-22 02:56:08 -0500

    Added redirect after retireve access token

--- Todays/js/handlers/Login.jsx
+++ Todays/js/handlers/Login.jsx
@@ -5,7 +5,7 @@ class Login {
     _login() {
         const clientId = '876ce3c2be4c4ae584eea907ffeb6e0b';
         const scopes = 'user-read-private playlist-modify-public playlist-modify-private';
-        const redirectUri = 'http://localhost:5500/view/playlist/';
+        const redirectUri = 'http://localhost:5500/bit';
         window.location.replace('https://accounts.spotify.com/authorize' +
         '?client_id=' + clientId +
         '&redirect_uri=' + encodeURIComponent(redirectUri) +
